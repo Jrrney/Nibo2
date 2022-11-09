29d2ad4f1943da6121aad97f2ea6310e95282b7c
Valentin Schröter <vasc9380@th-wildau.de>
2022-11-09 07:43:39 -0800

    popo

--- TomAndJerry/main.c
+++ TomAndJerry/main.c
@@ -4,12 +4,48 @@
  * @version
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // used for random
+
 #include "main.h"
+
+typedef enum direction {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	COUNT
+} t_direction;
+
+char randomDirection() {
+	char directionCode;
+	// test rand() !!!
+	// t_direction d = rand();
+
+}
+
+char generateManoveurByte(int minDistance,
+								int maxDistance,
+								int minSpeed,
+								int maxSpeed) {
+	char manoveurByte = "10101010"
+	manoveurByte = randomDirection();
+	manoveurByte = manoveurByte + randomDistance(minDistance, maxDistance);
+	manoveurByte = manoveurByte + randomSpeed(minSpeed, maxSpeed);
+
+	return manoveurByte;
+}
 
 /**
  * Main Method of the program.
  */
 int main() {
+
+	// using system time for setting a seed for pseudo-random number generation
+	srand(time(0));
+
+	rand()
 
 }
 
