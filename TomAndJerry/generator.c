498577a2e2d166cd32b2da00fc3b95fffc1804ac
Valentin Schröter <vasc9380@th-wildau.de>
2022-11-22 13:47:06 -0800

    add generator method-implementations

--- TomAndJerry/generator.c
+++ TomAndJerry/generator.c
@@ -1,4 +1,32 @@
 #include "main.h"
 #include "generator.h"
 
+t_movement generateMove() {
+	t_movement move;
 
+	move.direction = getRandomDirection();
+	move.ticks = randomInRange(1, 7);
+	move.speed = randomInRange(1, 7);
+
+	return move;
+}
+
+t_movement * generateMoveList() {
+	static t_movement moveList[randomInRange(MIN_MOVES, MAX_MOVES)];
+	int numberOfMoves = sizeof(moveList)/sizeof(t_movement);
+
+	for (int i = 0; i < numberOfMoves; i++) {
+		moveList[i] = generateMove();
+	}
+
+	return moveList;
+}
+
+t_direction getRandomDirection() {
+	t_direction rDirection = randomInRange(0, COUNT - 1);
+	return rDirection;
+}
+
+int randomInRange(int lower, int upper) {
+	return (rand() % (upper - lower + 1)) + lower;
+}
